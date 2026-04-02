f266fa60216efc95e066b2e974a1563e69ac1e8c
zhansaya077 <zhansaya.bekbulat@narxoz.kz>
2026-04-02 16:59:48 +0500

    Refactor handle method parameters in HpHandle

--- src/com/narxoz/rpg/chain/HpHandler.java
+++ src/com/narxoz/rpg/chain/HpHandler.java
@@ -2,11 +2,10 @@ package com.narxoz.rpg.chain;
 
 import com.narxoz.rpg.arena.ArenaFighter;
 
-   public class HpHandler extends DefenseHandler {
+public class HpHandler extends DefenseHandler {
 
     @Override
-    public void handle(int incomingDamage, ArenaFighter target) {
-        target.takeDamage(incomingDamage);
+    public void handle(int damage, ArenaFighter target) {
+        target.takeDamage(damage);
     }
 }
-
