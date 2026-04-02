b737c5119725f1ea4a9f8c06225ef6d426113307
zhansaya077 <zhansaya.bekbulat@narxoz.kz>
2026-04-02 16:58:15 +0500

    Update BlockHandler.java
    Refactor BlockHandler to calculate damage reduction based on blockPercent.

--- src/com/narxoz/rpg/chain/BlockHandler.java
+++ src/com/narxoz/rpg/chain/BlockHandler.java
@@ -3,18 +3,15 @@ package com.narxoz.rpg.chain;
 import com.narxoz.rpg.arena.ArenaFighter;
 
 public class BlockHandler extends DefenseHandler {
-    private final double blockPercent;
+    private double blockPercent;
 
     public BlockHandler(double blockPercent) {
         this.blockPercent = blockPercent;
     }
 
     @Override
-    public void handle(int incomingDamage, ArenaFighter target) {
-        // TODO: Calculate how much damage is blocked: (int)(incomingDamage * blockPercent).
-        // TODO: Subtract the blocked amount from incomingDamage to get the remainder.
-        // TODO: Print a block message showing how much was blocked.
-        // TODO: Always pass the remainder to the next handler (block reduces but never stops the chain).
-        // Design question: what should happen if the remainder reaches 0 or below?
+    public void handle(int damage, ArenaFighter target) {
+        int reduced = (int)(damage * (1 - blockPercent));
+        passToNext(reduced, target);
     }
 }
